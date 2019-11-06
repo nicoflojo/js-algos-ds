35a0ae49b283ae3d9235615760833a29ac448b58
nicoflojo <nico@pop-os.localdomain>
2019-11-06 07:57:45 +0700

    adds bubblesort es6

---
+++ algorithms/sorting/bubbleSort.js
@@ -0,0 +1,26 @@
+// Bubble Sort Implemneted in JS
+import Sort from '../Sort';
+
+export default class BubbleSort extends Sort {
+	sort(originalArray) {
+		let swapped = false;
+		const array = [...originalArray];
+		for (let i = 1; i < array.length; i += 1) {
+			swapped = false;
+			this.callbacks.visitingCallback(array[i]);
+			for (let j = 0; j < array.length - 1; j +=1) {
+				this.callbacks.visitingCallback(array[j]);
+
+				if (this.comparator.lessThan(array[j + 1])) {
+					[array[j], array[j + 1] = array[j + 1], array[j]];
+
+					swapped = true;
+				}
+			}
+			if (!swapped) {
+				return array;
+			}
+		}
+		return array;
+	} 
+} 
